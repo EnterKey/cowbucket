8af1d937f4b8630029ca6496dfb2d58353cae568
엄두성 <enterkey88@gmail.com>
2014-08-22 16:30:34 +0900

    change mainPage

--- public/javascript/common.js
+++ public/javascript/common.js
@@ -7,6 +7,24 @@ common.setEventListener = function() {
 		e.preventDefault();
 		window.location.replace("/app");
 	});
+	
+	// $('.contents > div').on('mouseover', function(e) {
+// 		
+	// });
+	
+	$('#auto-mode').on('mouseover', function() {
+		if(!$('#auto-mode').hasClass('active-div')) {
+			$('#unauto-mode').removeClass('active-div');
+			$('#auto-mode').addClass('active-div');
+		}
+	});
+	
+	$('#unauto-mode').on('mouseover', function() {
+		if(!$('#unauto-mode').hasClass('active-div')) {
+			$('#auto-mode').removeClass('active-div');
+			$('#unauto-mode').addClass('active-div');
+		}
+	});
 };
 	
 
