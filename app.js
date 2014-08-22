5bfb4c1bef5cce9e346f4759a0dc23b1c4844981
엄두성 <enterkey88@gmail.com>
2014-08-22 13:35:56 +0900

    init cowbucket

---
+++ app.js
@@ -0,0 +1,62 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var ejs = require('ejs');
+
+var app = require('express')(), server = require('http').createServer(app);
+
+// all environments
+// app.set('port', process.env.PORT || 3000);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+// app.use(express.favicon());
+// app.use(express.logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.methodOverride());
+// app.use(app.router);
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//setting
+app.configure(function () {
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.engine('.html', ejs.__express);
+	ejs.open = '<?';
+	ejs.close = '?>';
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.urlencoded());
+	app.use(express.json());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('keyboard cat'));
+	app.use(express.session());
+	app.use(express.static(path.join(__dirname, '/public')));
+	app.use(function (req, res, next) {
+		res.locals.session = req.session;
+		next();
+	});
+});
+
+// development only
+if ('development' === app.get('env')) {
+	app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+app.use(app.router);
+
+server.listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
+});
